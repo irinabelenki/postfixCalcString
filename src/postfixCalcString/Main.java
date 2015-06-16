69cce693a00b62511e2e40841a48ef1b9fe7e4ef
Irina Belenki <irina.belenki@gmail.com>
2015-06-16 22:01:38 +0300

    Fixed errors.

--- src/postfixCalcString/Main.java
+++ src/postfixCalcString/Main.java
@@ -6,32 +6,42 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static Stack<Integer> operandStack = new Stack<Integer>();
+	private static Stack<Integer> operandStack = new Stack<Integer>();	
+	
+	public enum Operation {		 
+		PLUS("+"), 
+		MINUS("-"), 
+		MULTIPLY("*"), 
+		DIVIDE("/");
+	 
+		private String content;
+	 
+		private Operation(String content) {
+			this.content = content;
+		}
+	 
+		public String getContent() {
+			return content;
+		}	 
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Enter line in postfix format");
+		
 		try {
 			String line = new BufferedReader(new InputStreamReader(System.in)).readLine();
 			StringTokenizer elementsList = new StringTokenizer(line);
 			
 			while (elementsList.hasMoreTokens()) {
 				String element = elementsList.nextToken();
-				boolean operandFound = false;
-				boolean operationFound = false;
 				try {
 					int operand = Integer.parseInt(element);
 					operandStack.push(operand);
-					operandFound = true;
 					continue;
 				} catch (NumberFormatException nfe) {
 				}
 				
-				String operation = element;
-				if (containsOperation(operation)) {
-					operationFound = true;
-				}
-
-				if (!operandFound && !operationFound) {
+				if (!containsOperation(element)) {
 					System.out.println("Invalid character: " + element);
 					break;
 				}
@@ -43,17 +53,17 @@ public class Main {
 
 				int result = 0;
 				int secondOperand = 0;
-				if (operation.equals("+")) {
+				if (element.equals("+")) {
 					result = operandStack.pop() + operandStack.pop();
 				}
-				else if(operation.equals("*")) {
+				else if(element.equals("*")) {
 					result = operandStack.pop() * operandStack.pop();
 				}
-				else if(operation.equals("-")) {
+				else if(element.equals("-")) {
 					secondOperand = operandStack.pop();
 					result = operandStack.pop() - secondOperand;
 				}
-				else if(operation.equals("/")) {
+				else if(element.equals("/")) {
 					secondOperand = operandStack.pop();
 					if (secondOperand == 0) {
 						System.err.println("Cannot divide by zero");
@@ -63,21 +73,18 @@ public class Main {
 				}
 				operandStack.push(result);
 				System.out.println("Result: " + result);
-				operationFound = true;
-
 			}
-		} catch (Exception e) {
+		} 
+		catch (Exception e) {
 			System.err.println("Exception:" + e.getMessage());
 		}
-
 	}
 
 	public static boolean containsOperation(String operation) {
-		if (operation.equals("+") || 
-			operation.equals("-") || 
-			operation.equals("*") || 
-			operation.equals("/")) {
-			return true;
+		for(Operation value : Operation.values()) {
+			if (value.getContent().equals(operation)) {
+				return true;
+			}
 		}
 		return false;
 	}
